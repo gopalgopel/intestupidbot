1df8cce8cd34174c722fb26a3f0cad4815980565
gopalgopel <gopaldroger@gmail.com>
2017-06-08 13:49:44 +0700

    nambahin pelaporan lokasi buat DEMO ONLY

--- intelebot.js
+++ intelebot.js
@@ -39,14 +39,80 @@ io.listen(2999);
 // GLOBAL VAR // 
 // var PENERIMA_LAPORAN = [165983450,4117519]; // penerima klo ada laporan masuk, 
 var PENERIMA_LAPORAN = [4117519]; // penerima klo ada laporan masuk, 
-
+var BUFFID, BUFFDARI, BUFFKATEG, BUFFPESAN;
 
 
 ////////////////// START! //////////////////
+// {
+//   "_id": "5938e8ad62ec267c27000002",
+//   "laporan": "testing #lokasi buat demo nanti",
+//   "category": "lokasi",
+//   "date": "2017-06-08T06:03:23.000Z",
+//   "type": "private",
+//   "dari": "@gopalgopel",
+//   "__v": 0
+// }
 
 // behaviour klo nerima message 
 api.on('message', function(message)
 {
+	// lokasi
+	if (message.location){
+    	console.log(message.location);
+
+    	pesan = {
+    		lokasi: message.location, 
+    		pesan_id: BUFFID 
+	   	};
+
+		var args = {
+		    data: pesan,
+		    headers: { "Content-Type": "application/json" }
+		};
+    	// kirim ke API
+    	client.put("http://localhost:9099/api/rawpesans/"+BUFFID, args, function (data, response) {
+		    // console.log(data);
+		    // console.log(response);
+		    console.log('data: ', data);
+		    // console.log('response: ', response);
+		    
+		    
+		    if (data.message.indexOf("updated") >= 0){
+			    api.sendMessage({
+		    		chat_id: message.chat.id,
+					text: 'Lokasi berhasil disimpan. Laporan sempurna. Terimakasih',
+					parse_mode: 'HTML'
+				});
+				//nambah minta lokasi
+		    } else {
+		    	api.sendMessage({
+					chat_id: message.chat.id,
+					text: 'lokasi gagal disimpan, '+data.message,
+					parse_mode: 'HTML'
+				});
+		    }
+		
+		// ngirim pesan realtime via socket.io
+		var soketiopesan = {
+			dari: BUFFDARI,
+		    kategori: BUFFKATEG,
+		    isi: BUFFPESAN,
+		    lokasi: message.location
+		};
+		console.log("ngirim pesan baru via soket:", soketiopesan)
+		io.emit('pesanintelbaru',soketiopesan);
+
+		 //    for (i=0; i<PENERIMA_LAPORAN.length; i++){
+		 //    	api.sendMessage({
+			// 		chat_id: PENERIMA_LAPORAN[i],
+			// 		text: '<b>'+data.message+'</b>\noleh '+message.chat.id +' / '+dari,
+			// 		parse_mode: 'HTML'
+			// 	})
+			// }
+		});
+    } 
+
+    // NORMAL MESSAGE
 	// parsing message
 	var buff = message.text.split(" ");
 	
@@ -67,13 +133,17 @@ api.on('message', function(message)
 			  catg.push(match[1]);
 			}
 		}
-		// ngambil hashtag
 		
 		type = message.chat.type;
 		d = new Date(message.date * 1000);
 		isi = message.text.replace('/lapor ','');
 
-	    pesan = {
+		// nyimpen ke var buffer global
+		BUFFDARI = dari; 
+		BUFFKATEG  = catg;
+		BUFFPESAN = isi;
+
+		pesan = {
     		dari: dari, 
     		type: type,
     		date: d,
@@ -86,16 +156,30 @@ api.on('message', function(message)
 		    headers: { "Content-Type": "application/json" }
 		};
 		 
-		client.post("http://192.168.1.241:9099/api/rawpesans/", args, function (data, response) {
-		    console.log(data);
+		// client.post("http://192.168.1.241:9099/api/rawpesans/", args, function (data, response) {
+		client.post("http://localhost:9099/api/rawpesans/", args, function (data, response) {
+		    // console.log(data);
 		    // console.log(response);
+		    console.log('data: ', data);
+		    // console.log('response: ', response);
+		    
 		    
 		    if (data.message.indexOf("berhasil") >= 0){
+
+		    	var s = data.message.search("_id:");
+		    	var n = data.message.search(" } berhasil");
+		    	s=s+5;
+				BUFFID = data.message.substring(s, n);		    	
+
+		    	{ message: 'pesan { __v: 0,\n  laporan: \'hyygd\',\n  category: null,\n  date: Thu Jun 08 2017 12:09:06 GMT+0700 (WIB),\n  type: \'private\',\n  dari: \'@gopalgopel\',\n  _id: 5938dbf3e9acae5b1d000015 } berhasil digenerate!' }
+
 			    api.sendMessage({
 					chat_id: message.chat.id,
-					text: 'laporan berhasil disimpan',
+					text: 'Laporan berhasil disimpan.\nKirim lokasi kejadian / lokasi anda dari menu: '+
+						  '\n<i>attach --> location</i>',
 					parse_mode: 'HTML'
 				});
+				//nambah minta lokasi
 		    } else {
 		    	api.sendMessage({
 					chat_id: message.chat.id,
@@ -105,8 +189,8 @@ api.on('message', function(message)
 		    }
 		
 		// ngirim pesan realtime via socket.io
-		console.log("ngirim pesan baru via soket:", args)
-		io.emit('pesanintelbaru',args);
+		// console.log("ngirim pesan baru via soket:", args)
+		// io.emit('pesanintelbaru',args);
 
 		 //    for (i=0; i<PENERIMA_LAPORAN.length; i++){
 		 //    	api.sendMessage({
